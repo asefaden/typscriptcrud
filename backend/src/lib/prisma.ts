1d473076a2793005a66351edcd05144c1a2b899f
Assefa Demeke <assede1122@gmail.com>
2026-06-08 19:08:05 +0300

    fix: ensure next build runs before start to prevent production build error

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,7 +2,7 @@
 // import { PrismaClient } from '@prisma/client';
 
 // 👇 በፕሪስማ 7 አሰራር መሰረት በዚህ አዲሱ መስመር ይተኩት፦
-import { PrismaClient } from '../../prisma/generated/client'; 
+import { PrismaClient } from '@prisma-client'; 
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
