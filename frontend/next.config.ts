25cdb079a60f85cc3a3d338e544c39d17f81b15f
Assefa Demeke <assede1122@gmail.com>
2026-06-08 12:49:39 +0300

    update next.config.ts to point to deployed backend and prisma studio

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,25 +5,26 @@ const nextConfig = {
       {
         // Proxy task API requests to the backend
         source: '/api/tasks',
-        destination: 'http://localhost:5000/api/tasks',
+        //destination: 'http://localhost:5000/api/tasks',
+        destination: 'https://typescript.app.aletcloud.com/api/tasks',
       },
       {
         source: '/api/tasks/:path*',
-        destination: 'http://localhost:5000/api/tasks/:path*',
+        destination: 'https://typescript.app.aletcloud.com/api/tasks/:path*',
       },
       {
         // 2. ለሎጊን እና ለምዝገባ APIዎች ማስተላለፊያ
         source: '/api/auth/:path*',
-        destination: 'http://localhost:5000/api/auth/:path*',
+        destination: 'https://typescript.app.aletcloud.com/api/auth/:path*',
       },
       {
         // Proxy Prisma Studio requests
         source: '/prisma-admin',
-        destination: 'http://localhost:5555',
+        destination: 'https://typescript.app.aletcloud.com/prisma-admin',
       },
       {
         source: '/prisma/:path*',
-        destination: 'http://localhost:5555/:path*',
+        destination: 'https://typescript.app.aletcloud.com/prisma/:path*',
       }
     ];
   },
